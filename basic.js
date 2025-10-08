435edce65ac7e222a2a2b676248e321f756ab62c
skepttalk <kuldeepsingh861915@gmail.com>
2025-10-08 19:42:32 +0530

    feat:js-logic-building-question

---
+++ basic.js
@@ -0,0 +1,18 @@
+for(let i=1;i<=10;i++){
+  console.log(i)
+}
+
+ 
+
+for(let i=100;i>=1;i--){
+  console.log(i)
+}
+
+
+
+let Table = []
+
+for (let i=1;i<=10;i++){
+    Table = i*2;
+  console.log(Table)
+}
---
+++ checksame.js
@@ -0,0 +1,16 @@
+let arr   = [1,2,3,5,8]
+let arr1  = [1,2,3,5,8]
+
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i] !== arr1[i]){
+         console.log("array not eu")
+         break;
+    }  
+}
+console.log("equal")
+
+
+
+
+
